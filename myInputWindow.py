117cd6209cf1af50cd78534afe2082544e6fb5ce
izumi0x01 <izumisoh.develop@gmail.com>
2022-11-26 14:31:43 +0900

    入力されるデータを監視するクラスの追加

--- myInputWindow.py
+++ myInputWindow.py
@@ -2,11 +2,15 @@ import tkinter as tk
 
 
 class InputWindow(tk.Frame):
-    def __init__(self, root):
+
+    def __init__(self, root, inputPressure: float = 0.0, inputDeltaTime: int = 0):
         super().__init__(root)
 
         root.option_add('*font', 'ＭＳゴシック 22')
         root.title("装置との通信用窓")
+
+        # ウィンドウの閉じるボタンは無効にする
+        root.protocol('WM_DELETE_WINDOW', (lambda: 'pass')())
 
         # 窓全体の設定
         self.tf = tk.Frame(root)
@@ -54,6 +58,34 @@ class InputWindow(tk.Frame):
 
     def InputDeltaTimeHandler():
         pass
+
+
+class ValueStrager():
+    def __init__(self):
+        self.__pressure: float = 0.0
+        self.__deltatime: int = 0
+
+    # # 引数にクラスオブジェクトを入れると、勝手に変数を文字列に変換してくれるやつ
+    # def __str__(self):
+    #     return self.message
+
+    @property
+    def Pressure(self):
+        return self.__pressure
+
+    @Pressure.setter
+    def Pressure(self, value):
+        if value != '':
+            self.__pressure = value
+
+    @property
+    def DeltaTime(self):
+        return self.__deltatime
+
+    @DeltaTime.setter
+    def DeltaTime(self, value):
+        if value != '':
+            self.__deltatime = value
 
 
 if __name__ == '__main__':
