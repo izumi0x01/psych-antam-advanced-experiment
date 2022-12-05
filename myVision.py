16d56b2e21fb0846479152a3d5b8520d4af8bcb5
izumi0x01 <izumisoh.develop@gmail.com>
2022-12-05 21:57:18 +0900

    白黒画像上に矩形の表示ができた

--- myVision.py
+++ myVision.py
@@ -1,14 +1,16 @@
 from ast import While
 from turtle import update
+from cv2 import contourArea
 import unicurses
+import numpy as np
 import cv2
 import sys
 
 
 class Vision:
 
-    X, Y = 120, 40
-    H, W = 520, 440
+    X0, Y0 = 0, 0
+    X1, Y1 = 0, 0
     FPS = 10
     MAIN_WINDOW_NAME = "RAW"
     MASKED_WINDOW_NAME = "Masked"
@@ -20,27 +22,77 @@ class Vision:
     def MakeWindow(self):
         cv2.namedWindow(self.MASKED_WINDOW_NAME)
         cv2.createTrackbar("slesh_hold", self.MASKED_WINDOW_NAME, self.__threshold,
-                           255, self.TrackBarChangedEvent)
+                           255, self.SleshHoldChangedEventHandler)
         self.__cam = cv2.VideoCapture(1+cv2.CAP_DSHOW)  # 引数に番号だけ渡すのではダメだった模様
         if not self.__cam.isOpened():
             return print("failure video capture")
         self.__cam.set(cv2.CAP_PROP_FPS, self.FPS)
+        cv2.createTrackbar("height", self.MASKED_WINDOW_NAME, 0,
+                           int(self.__cam.get(cv2.CAP_PROP_FRAME_HEIGHT) * 0.5), self.Y0ChangedEventHandler)
+        cv2.createTrackbar("width", self.MASKED_WINDOW_NAME, 0,
+                           int(self.__cam.get(cv2.CAP_PROP_FRAME_WIDTH) * 0.5), self.X0ChangedEventHandler)
 
-    def TrackBarChangedEvent(self, position):
+    def SleshHoldChangedEventHandler(self, position):
         self.__threshold = cv2.getTrackbarPos(
             "slesh_hold", self.MASKED_WINDOW_NAME)
 
+    def X0ChangedEventHandler(self, position):
+        self.X0 = cv2.getTrackbarPos("width", self.MASKED_WINDOW_NAME)
+
+    def Y0ChangedEventHandler(self, position):
+        self.Y0 = cv2.getTrackbarPos("height", self.MASKED_WINDOW_NAME)
+
+    def X1ChangedEventHandler(self, position):
+        self.X1 = int(self.__cam.get(cv2.CAP_PROP_FRAME_WIDTH))
+        - cv2.getTrackbarPos("width", self.MASKED_WINDOW_NAME)
+
+    def Y1ChangedEventHandler(self, position):
+        self.Y1 = int(self.__cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        - cv2.getTrackbarPos("height", self.MASKED_WINDOW_NAME)
+
     def UpdateWindow(self):
-        ret, frame1 = self.__cam.read()
-        k = cv2.waitKey(1) & 0xFF  # 描画の待ち時間設定
-        frame = frame1[self.Y:self.Y+self.H,
-                       self.X:self.X+self.W]  # frame全体から画像の抽出
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        ret, Frame = self.__cam.read()
+        cv2.imshow(self.MAIN_WINDOW_NAME, Frame)
+        # 描画の待ち時間設定
+        k = cv2.waitKey(1) & 0xFF
+        if k == ord('q'):
+            sys.exe()
+        # frame全体から画像の抽出
+        # Frame = Frame1[self.Y0:self.Y1, self.X0:self.X1]
+        grayFrame = cv2.cvtColor(Frame, cv2.COLOR_BGR2GRAY)
         ret, mask_image = cv2.threshold(
-            gray, self.__threshold, 255, cv2.THRESH_BINARY_INV)
+            grayFrame, self.__threshold, 255, cv2.THRESH_BINARY_INV)
+        mask_image = self.GetRailCountour(mask_image)
+        # cv2.imshow(self.MASKED_WINDOW_NAME, mask_image)
+
+    def GetRailCountour(self, mask_image):
+        # contours, hierarchy = cv2.findContours(
+        #     grayFrame, cv2.RETR_TREE, cv2.CHAIN_APPROX_TC89_L1)
+        # for i, cnt in enumerate(contours):
+        #     # print(f"contours[{i}].shape: {cnt.shape}")
+        #     rect = cv2.minAreaRect(contours[i])
+        #     box = cv2.boxPoints(rect)
+        #     box = np.int0(box)
+        #     grayFrame = cv2.drawContours(grayFrame, [box], 0, (0, 0, 255), 2)
+        # return
+        contours, hierarchy = cv2.findContours(
+            mask_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        if len(contours) == 0:
+            return mask_image
+
+        rect = cv2.minAreaRect(contours[0])
+        box = cv2.boxPoints(rect)
+        box = np.int0(box)
+        print(box)
+        mask_image = cv2.cvtColor(mask_image, cv2.COLOR_GRAY2BGR)
+        mask_image = cv2.drawContours(mask_image, [box], 0, (0, 0, 255), 10)
+        # cv2.rectangle(mask_image, (384, 0), (510, 128), (0, 255, 255), 3)
 
         cv2.imshow(self.MASKED_WINDOW_NAME, mask_image)
-        cv2.imshow(self.MAIN_WINDOW_NAME, frame)
+        return mask_image
+
+    def CalcCenterOfGravity(self):
+        pass
 
     def getDistance(self):
         return self.distance
